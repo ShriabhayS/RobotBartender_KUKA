ff4aea0967c2472e3244db5f45350c9821dab672
Na3N <121786716+Na3N@users.noreply.github.com>
2025-09-16 21:24:52 +1000

    added current robot project, and updated ui code
    bartender.java is the robot code, currently has all opcua enabled. when bartender.java works properly we can switch to motiontest to test paths. also the ui code seemed to not work, added some fixes and works now

--- ui stuff lol/mybrainisfried.py
+++ ui stuff lol/mybrainisfried.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask, render_template
 from asyncua import Client
 from asyncua import ua
@@ -26,25 +27,35 @@ url = "opc.tcp://172.24.200.1:4840"
 async def main(val):
     print(f"Connecting to {url} ...")
     async with Client(url=url) as client:
-        # Get the variable node using the path
-        var = await client.nodes.root.get_child(["0:Objects", "21:robot1", "0:R1c_int1"])
-        print(f"Variable Node: {var}")
+
+        # while True:
+        #     var_ready = await client.nodes.root.get_child(["0:Objects", "21:robot1", "0:R1f_ready"])
+        #     if var_ready == "true":
+        #         break
+        #     else:
+        #         time.sleep(1)
+        #         print(var_ready)
+        # Get the variable nodes using the path
+        var_int = await client.nodes.root.get_child(["0:Objects", "21:robot1", "0:R1c_int1"])
+        var_start = await client.nodes.root.get_child(["0:Objects", "21:robot1", "0:R1c_Start"])
 
         # Read the current value and data type
-        value = await var.read_value()
-        data_type = await var.read_data_type()
-        print(f"Current value of MyVariable ({var}): {value}")
+        value = await var_int.read_value()
+        data_type = await var_int.read_data_type()
+        print(f"Variable Node: {var_int}")
+        print(f"Current value of MyVariable ({var_int}): {value}")
         print(f"Expected data type: {data_type}")
-
-        var = await client.nodes.root.get_child(["0:Objects", "21:robot1", "0:R1c_Start"])
-        await var.write_value(True)
 
         # Set the new value back to the server
         new_value = ua.Variant(val, ua.VariantType.Int32)  # Ensure the value matches the expected Int32 type
         print(f"Setting value of MyVariable to {new_value} ...")
-        await var.write_value(new_value)
+        await var_int.write_value(new_value)
 
         print(f"Value successfully set to {new_value}.")
+
+        # Turn on start
+        await var_start.write_value(True)
+        print(f"Enable successfully set to True")
 
 if __name__ == "__main__":
     app.run()
